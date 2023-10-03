b77466e8e11759e41ac28815cbc1b026bea36149
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-10-03 04:47:04 +0100

    Basically completed project

--- ExpenseApp/models.py
+++ ExpenseApp/models.py
@@ -1,14 +1,18 @@
+import datetime
 from django.db import models
 from django.utils.translation import gettext as _ 
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Expense(models.Model):
+
     
     name = models.CharField(_(""), max_length=100)
     amount = models.DecimalField(_(""), max_digits=10, decimal_places=2)
     category = models.CharField(_(""), max_length=50)
-    created_date = models.DateTimeField(_("") ,blank=True, null=True
+    owner = models.ForeignKey(User, verbose_name=_(""), on_delete=models.CASCADE, blank=True, null=True)
+    created_date = models.DateTimeField(_("") ,blank=True, null=True, default= datetime.datetime.now()
                                         # , auto_now_add=True
                                         )
     updated_date = models.DateTimeField(_(""), auto_now=True)
--- ExpenseApp/views.py
+++ ExpenseApp/views.py
@@ -4,55 +4,65 @@ from django.contrib import messages
 from django.db.models import Sum
 from django.utils import timezone
 import datetime
+import json
 # Create your views here.
 
-categories = [{'miscellaneous': 'Miscellaneous', 'food': 'Food', 'shoes': 'Shoes', 'wears': 'Wears'}]
-
+categories = ["Miscellaneous", "Food", "Shoes", "Wears", "Tax", 'Bills']
 
 
 def index(request):
-
     expense = None
 
     if request.GET.get('edit_id'):
-        expense= get_object_or_404(Expense, id = int(request.GET['edit_id']))
+        expense= get_object_or_404(Expense, id = int(request.GET['edit_id']), owner = request.user)
 
     if request.method == 'POST':
         name = request.POST['expense_name']
         amount = request.POST['expense_amount']
         category = request.POST['expense_category']
 
+        if not request.user.is_authenticated:
+            messages.error(request, 'You are not logged in!')
+            return redirect('/')
+
+        if category == "":
+            messages.warning(request, 'No category selected')
+            return redirect('/')
+
         if request.GET.get('edit_id'):
             expense= get_object_or_404(Expense, id = int(request.GET['edit_id']))
             expense.amount = request.POST.get('expense_amount')
             expense.category = request.POST.get('expense_category')
             expense.name = request.POST.get('expense_name')
+            expense.owner = request.user
 
             expense.save()
             messages.success(request, 'Expense record updated successfully.')
             return redirect('/')
         
-        Expense.objects.create(name = name, amount= amount, category = category)
+        Expense.objects.create(name = name, amount= amount, category = category, owner= request.user)
         messages.success(request, 'New Expense Record Added.')
         return redirect('/')
     
-    expenses = Expense.objects.all()
-    expense_total = Expense.objects.all().aggregate(expense_total= Sum('amount'))
+    expenses = Expense.objects.filter(owner = request.user)
+    expense_total = expenses.aggregate(expense_total= Sum('amount'))
     
     daays = datetime.datetime.today()
-
-    last_7_days = Expense.objects.filter(created_date__gte = str(daays - datetime.timedelta(days= 7))).all().aggregate(total_7_days = Sum('amount'))
-    last_30_days = Expense.objects.filter(created_date__gte = str(daays - datetime.timedelta(days= 30))).all().aggregate(total_30_days = Sum('amount'))
-    last_52_weeks = Expense.objects.filter(created_date__gte = str(daays - datetime.timedelta(weeks= 52))).all().aggregate(total_52_weeks = Sum('amount'))
+    past_30_days_sum_expenses = expenses.values('created_date').annotate(sum= Sum('amount'))
+    categorical_expenses = expenses.values('category').annotate(sum= Sum('amount'))
+    
+    last_7_days = expenses.filter(created_date__gte = str(daays - datetime.timedelta(days= 7))).aggregate(total_7_days = Sum('amount'))
+    last_30_days = expenses.filter(created_date__gte = str(daays - datetime.timedelta(days= 30))).aggregate(total_30_days = Sum('amount'))
+    last_52_weeks = expenses.filter(created_date__gte = str(daays - datetime.timedelta(weeks= 52))).aggregate(total_52_weeks = Sum('amount'))
     context = {'expenses': expenses, 'total': expense_total, 'last_7_days': last_7_days, 'last_30_days': last_30_days, 
-               'last_52_weeks': last_52_weeks, 'categories': categories, 'expense': expense}
-
+    'last_52_weeks': last_52_weeks, 'categories': categories, 'expense': expense, 'past_30_days_sum_expenses': past_30_days_sum_expenses,  'categorical_expenses': categorical_expenses}
+    
     return render(request, 'index.html', context)
 
 
 def delete(request, id):
 
-    expense = get_object_or_404(Expense, id = id)
+    expense = get_object_or_404(Expense, id = id, owner= request.user)
     expense.delete()
     messages.success(request, 'Expense record deleted successfully.')
     return redirect('/')
